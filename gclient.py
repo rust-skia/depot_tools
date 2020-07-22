33e6e046e0e3a1c986c35e40ce509b1e1a135528
Corentin Wallez <cwallez@chromium.org>
2020-07-22 14:44:34 +0000

    gclient: print use_relative_hooks deprecation on stderr.
    Printing to stdout breaks tools that parse the output of running
    gclient.
    
    TBR=ehmaldonado@chromium.org
    Bug: chromium:1107325
    Bug: chromium:1108301
    Change-Id: I77f6eef9bdffd0534efb0b919304dab5b3df8c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/2312678
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Auto-Submit: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Michael Achenbach <machenbach@chromium.org>

--- gclient.py
+++ gclient.py
@@ -795,7 +795,7 @@ class Dependency(gclient_utils.WorkItem, DependencySettings):
     # compute which working directory should be used for hooks
     if local_scope.get('use_relative_hooks', False):
       print('use_relative_hooks is deprecated, please remove it from DEPS. ' +
-            '(it was merged in use_relative_paths)')
+            '(it was merged in use_relative_paths)', file=sys.stderr)
 
     hooks_cwd = self.root.root_dir
     if self._use_relative_paths:
