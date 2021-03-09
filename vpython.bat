0e8b3107e056f22f04ed82d6906560e7dd83baae
Josip Sokcevic <sokcevic@google.com>
2021-03-09 21:53:47 +0000

    Remove py batch wrappers from root dir
    This removes python.bat and python3.bat batch wrappers from root of this
    repository. This change brings Windows set up closer to unix.
    
    depot_tools provides batch wrappers in python-bin and python2-bin
    directories and user can opt in to use it by adding them to PATH.
    
    Bug: 777069
    Change-Id: Ie7571a49f940e7a69b6c18779499c39aee94d06a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/2742181
    Reviewed-by: Edward Lesmes <ehmaldonado@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Josip Sokcevic <sokcevic@google.com>

--- vpython.bat
+++ vpython.bat
@@ -3,5 +3,10 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
+:: TODO(crbug.com/1003139): Remove.
+:: Add Python 3 to PATH to work around crbug.com/1003139.
+for /f %%i in (%~dp0python_bin_reldir.txt) do set PYTHON2_BIN_RELDIR=%%i
+set PATH=%~dp0%PYTHON2_BIN_RELDIR%;%~dp0%PYTHON2_BIN_RELDIR%\Scripts;%~dp0%PYTHON2_BIN_RELDIR%\DLLs;%PATH%
+
 call "%~dp0\cipd_bin_setup.bat" > nul 2>&1
-"%~dp0\.cipd_bin\vpython.exe" -vpython-interpreter "%~dp0\python.bat" %*
+"%~dp0\.cipd_bin\vpython.exe" -vpython-interpreter "%~dp0\%PYTHON2_BIN_RELDIR%\python.exe" %*
