ed6aa2b0e87048e0953e68f951437e97808e7339
Josip Sokcevic <sokcevic@google.com>
2022-01-26 18:14:05 +0000

    Reland "Use py3 in bot_update and gclient recipe_modules"
    This is a reland of bb88492ff3aa002d49b505738b3f488956e679cb
    
    Original change's description:
    > Use py3 in bot_update and gclient recipe_modules
    >
    > Recipe-Nontrivial-Roll: build
    > Recipe-Nontrivial-Roll: build_limited
    > Recipe-Nontrivial-Roll: chrome_release
    > Recipe-Nontrivial-Roll: chromiumos
    > Recipe-Nontrivial-Roll: infra
    > Bug: 1289280
    > Change-Id: Id61bacf4acef319f7ea22161081b632677ca363c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/3402082
    > Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    > Reviewed-by: Gavin Mak <gavinmak@google.com>
    > Commit-Queue: Josip Sokcevic <sokcevic@google.com>
    
    Recipe-Nontrivial-Roll: build
    Recipe-Nontrivial-Roll: build_limited
    Recipe-Nontrivial-Roll: chrome_release
    Recipe-Nontrivial-Roll: chromiumos
    Recipe-Nontrivial-Roll: infra
    Bug: 1289280, 1289504
    Change-Id: I2f2f7244568b1fdfb960d5cdfa3937175ac1ed2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/3414742
    Reviewed-by: Gavin Mak <gavinmak@google.com>
    Reviewed-by: Yiwei Zhang <yiwzhang@google.com>
    Commit-Queue: Josip Sokcevic <sokcevic@google.com>

--- recipes/recipe_modules/bot_update/resources/bot_update.py
+++ recipes/recipe_modules/bot_update/resources/bot_update.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -231,7 +231,7 @@ def call(*args, **kwargs):  # pragma: no cover
       if not buf:
         break
       if hanging_cr:
-        buf = '\r' + buf
+        buf = b'\r' + buf
       hanging_cr = buf.endswith(b'\r')
       if hanging_cr:
         buf = buf[:-1]
