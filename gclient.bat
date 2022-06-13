544594e0b2760e2ded229d6b06782f01d82b0a6c
Aleksey Khoroshilov <akhoroshilov@brave.com>
2022-06-13 17:22:20 +0000

    Reland "Make depot_tools scripts return exit codes properly on all platforms."
    This reverts commit 6754c49e02b62d54b9b5d8b8b8a93e77bae935f8.
    
    Reason for revert: will be relanded with fixed batch comments that triggered parser error.
    
    Original change's description:
    > Revert "Make depot_tools scripts return exit codes properly on all platforms."
    >
    > This reverts commit 7c4f7ec40818ddd883aa0a3b132cea06af92a713.
    >
    > Reason for revert: bugs reported by users (see comments in Gerrit).
    > error: block. was unexpected at this time.
    >
    > Original change's description:
    > > Make depot_tools scripts return exit codes properly on all platforms.
    > >
    > > Changes:
    > > 1. Windows: exit /b %errorlevel% should be used instead of goto :EOF to get valid exit codes during cmd /c <script>.bat invocation.
    > > 2. Windows: delayed var expansion is required in update_depot_tools.bat exit code generation.
    > > 3. Posix: update_depot_tools returns exit code from update_git_repo function in case of a failure.
    > >
    > > A rule of thumb on Windows: goto :EOF should not be used if %errorlevel% must be returned for all possible invocations.
    > >
    > > Test case for update_depot_tools changes:
    > > 1. Make a change to depot_tools sources that will conflict with next depot_tools update
    > > 2. Run update_depot_tools either directly or via gclient
    > > 3. Expect a git error is triggered
    > > 4. Inspect %errorlevel% or $? depending on platform
    > > 5. Expected 1, but the actual result is 0.
    > >
    > > Test case for changes in other .bat files:
    > > 1. Make a change to depot_tools sources that will conflict with next depot_tools update
    > > 2. Run cmd /c gclient
    > > 3. Expect a git error is triggered
    > > 4. Inspect %errorlevel%
    > > 5. Expected 1, but the actual result is 0.
    > >
    > > Change-Id: I64459982bcd9cc3db1319a9b39224b7a7af8c5aa
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/3688632
    > > Reviewed-by: Josip Sokcevic <sokcevic@google.com>
    > > Commit-Queue: Josip Sokcevic <sokcevic@google.com>
    > > Auto-Submit: Aleksey Khoroshilov <akhoroshilov@brave.com>
    > > Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    >
    > Change-Id: I85d598af01d75588cdee77165d6af22270ee031d
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/3694139
    > Auto-Submit: Josip Sokcevic <sokcevic@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    
    Change-Id: I377f966ea1b1a567de815caca703b5e124a76b64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/3696396
    Reviewed-by: Josip Sokcevic <sokcevic@google.com>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Commit-Queue: Josip Sokcevic <sokcevic@google.com>
    Auto-Submit: Aleksey Khoroshilov <akhoroshilov@brave.com>

--- gclient.bat
+++ gclient.bat
@@ -10,8 +10,8 @@ IF "%DEPOT_TOOLS_UPDATE%" == "0" GOTO :CALL_GCLIENT
 :: Synchronize the root directory before deferring control back to gclient.py.
 call "%~dp0update_depot_tools.bat" %*
 :: Abort the script if we failed to update depot_tools.
-IF %errorlevel% NEQ 0 (
-  goto :EOF
+IF %ERRORLEVEL% NEQ 0 (
+  exit /b %ERRORLEVEL%
 )
 
 :CALL_GCLIENT
