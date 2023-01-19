c8f23e26bee90ce6b02211c9489d4e4e947af02a
Joanna Wang <jojwang@chromium.org>
2023-01-19 21:18:10 +0000

    Revert "Remove rietveld.cc to test removing all reitveld config sttuff."
    This reverts commit 8c5174b5276efa6375b3ff2f97ce2dab66ee335f.
    
    Reason for revert: Need to replace this before removing.
    
    Original change's description:
    > Remove rietveld.cc to test removing all reitveld config sttuff.
    >
    > Bug:b/266001713
    > Change-Id: Ib5342e7519d7f7eaccf0a0a6f4a39c13677406bb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4178919
    > Reviewed-by: Aravind Vasudevan <aravindvasudev@google.com>
    > Commit-Queue: Joanna Wang <jojwang@chromium.org>
    
    Bug: b/266001713
    Change-Id: Ief110063e7146f045df669ec138c4f01eccc44d0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4182421
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Aravind Vasudevan <aravindvasudev@google.com>
    Commit-Queue: Aravind Vasudevan <aravindvasudev@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Joanna Wang <jojwang@chromium.org>

--- tests/git_cl_test.py
+++ tests/git_cl_test.py
@@ -713,6 +713,7 @@ class TestGitCl(unittest.TestCase):
   def test_LoadCodereviewSettingsFromFile_gerrit(self):
     codereview_file = StringIO('GERRIT_HOST: true')
     self.calls = [
+      ((['git', 'config', '--unset-all', 'rietveld.cc'],), CERR1),
       ((['git', 'config', '--unset-all', 'rietveld.tree-status-url'],), CERR1),
       ((['git', 'config', '--unset-all', 'rietveld.viewvc-url'],), CERR1),
       ((['git', 'config', '--unset-all', 'rietveld.bug-prefix'],), CERR1),
