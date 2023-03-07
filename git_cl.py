8454fc2458b2421e0e339714c6ff7e6fffb70dc4
Bruce Dawson <brucedawson@chromium.org>
2023-03-07 22:15:25 +0000

    Remove Python 2 support for PRESUBMIT.py
    The presubmit system still supports invoking PRESUBMIT.py files using
    Python 2. This has recently been turned off on the bots so this change
    removes support more completely.
    
    There are still some python3 parameters being passed around - it seemed
    better to do the simplest possible removal now, with a follow-up change
    to remove more support code after this has sat for a while.
    
    Tests run from PRESUBMIT.py files could still be run using Python 2, but
    those should also have been addressed already. Removing support for that
    will be done in a subsequent change.
    
    Bug: 1207012
    Change-Id: Id244d547a04438f83734dba269c3cc180c148b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4315183
    Reviewed-by: Aravind Vasudevan <aravindvasudev@google.com>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>

--- git_cl.py
+++ git_cl.py
@@ -1435,21 +1435,11 @@ class Changelist(object):
             ' was not specified. To enable ResultDB, please run the command'
             ' again with the --realm argument to specify the LUCI realm.')
 
-    py3_results = self._RunPresubmit(args,
-                                     description,
-                                     use_python3=True,
-                                     resultdb=resultdb,
-                                     realm=realm)
-    if py3_results.get('skipped_presubmits', 1) == 0:
-      print('No more presubmits to run - skipping Python 2 presubmits.')
-      return py3_results
-
-    py2_results = self._RunPresubmit(args,
-                                     description,
-                                     use_python3=False,
-                                     resultdb=resultdb,
-                                     realm=realm)
-    return self._MergePresubmitResults(py2_results, py3_results)
+    return self._RunPresubmit(args,
+                              description,
+                              use_python3=True,
+                              resultdb=resultdb,
+                              realm=realm)
 
   def _RunPresubmit(self,
                     args,
--- tests/presubmit_unittest.py
+++ tests/presubmit_unittest.py
@@ -592,11 +592,6 @@ class PresubmitUnittest(PresubmitTestsBase):
             change=change, gerrit_obj=None, verbose=False))
     expected = (r'Running Python ' + str(sys.version_info.major) +
                 r' post upload checks \.\.\.\n')
-    if sys.version_info[0] == 2:
-      expected += ('Running .*PRESUBMIT.py under Python 2. Add USE_PYTHON3 = '
-                   'True to prevent this.\n')
-      expected += ('Running .*PRESUBMIT.py under Python 2. Add USE_PYTHON3 = '
-                   'True to prevent this.\n')
     self.assertRegexpMatches(sys.stdout.getvalue(), expected)
 
   def testDoPostUploadExecuterWarning(self):
@@ -628,17 +623,12 @@ class PresubmitUnittest(PresubmitTestsBase):
         presubmit.DoPostUploadExecuter(
             change=change, gerrit_obj=None, verbose=False))
 
-    extra = ''
-    if sys.version_info[0] == 2:
-      extra = ('Running .*PRESUBMIT.py under Python 2. Add USE_PYTHON3 = True '
-               'to prevent this.\n')
     expected = ('Running Python ' + str(sys.version_info.major) + ' '
                 'post upload checks \.\.\.\n'
-                '%s'
                 '\n'
                 '\*\* Post Upload Hook Messages \*\*\n'
                 '!!\n'
-                '\n' % extra)
+                '\n')
     self.assertRegexpMatches(sys.stdout.getvalue(), expected)
 
   def testDoPresubmitChecksNoWarningsOrErrors(self):
@@ -734,7 +724,6 @@ def CheckChangeOnCommit(input_api, output_api):
           }
         ],
         'more_cc': ['me@example.com'],
-        'skipped_presubmits': 0,
     }
 
     fake_result_json = json.dumps(fake_result, sort_keys=True)
