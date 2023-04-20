34f62b4af771c21b8c23cd0adbb3d1371ee58408
Josip Sokcevic <sokcevic@chromium.org>
2023-04-20 23:48:58 +0000

    Run post submit hooks on vpython3 if vpython is unavailable
    If bot doesn't have vpython, run post submit hooks with vpython3.
    presubmit_support already checks this too, and it will run those
    presubmits regardless of USE_PYTHON3 variable.
    
    Bug: 1411979
    Change-Id: Id7edb8a10c39d625154dcc5a131becad3d3ebcbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4457567
    Reviewed-by: Garrett Beaty <gbeaty@google.com>
    Commit-Queue: Josip Sokcevic <sokcevic@chromium.org>

--- git_cl.py
+++ git_cl.py
@@ -1514,7 +1514,7 @@ class Changelist(object):
     with gclient_utils.temporary_file() as description_file:
       gclient_utils.FileWrite(description_file, description)
       args.extend(['--description_file', description_file])
-      if not py3_only:
+      if not py3_only and os.getenv('LUCI_OMIT_PYTHON2') != 'true':
         p_py2 = subprocess2.Popen(['vpython', PRESUBMIT_SUPPORT] + args)
       p_py3 = subprocess2.Popen(['vpython3', PRESUBMIT_SUPPORT] + args +
                                 ['--use-python3'])
