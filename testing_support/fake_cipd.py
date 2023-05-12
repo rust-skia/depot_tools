d69302761b70d7bb660bcbd42b1395ee17b4f16b
Dan Le Febvre <dlf@google.com>
2023-05-12 00:41:52 +0000

    Reland "resolve CIPD package names in gclient"
    This CL relands the below CL with the addition of handling situations where the CIPD package name has a variable=value and does not equate to true. In this case just return the original value in DEPS.
    
    This is a reland of commit 8faa5514ec0c4a36d65b44acbd98b2eb66b91405
    
    Original change's description:
    > resolve CIPD package names in gclient
    >
    > Currently the package names are not resolved in gclient output, see example below.
    >
    > This is part of broader work to improve CIPD output, the next CL will replace 'None'.
    >
    > ```
    > $~ gclient revinfo -a | grep '${platform}\|${arch}'
    >
    > src/buildtools/linux64:gn/gn/linux-${arch}: None
    > src/buildtools/reclient:infra/rbe/client/${platform}: None
    > src/third_party/dawn/third_party/ninja:infra/3pp/tools/ninja/${platform}: None
    > src/third_party/dawn/tools/golang:infra/3pp/tools/go/${platform}: None
    > src/third_party/devtools-frontend-internal/devtools-frontend/third_party/esbuild:infra/3pp/tools/esbuild/${platform}: None
    > src/third_party/devtools-frontend/src/third_party/esbuild:infra/3pp/tools/esbuild/${platform}: None
    > src/third_party/ninja:infra/3pp/tools/ninja/${platform}: None
    > src/tools/luci-go:infra/tools/luci/isolate/${platform}: None
    > src/tools/luci-go:infra/tools/luci/swarming/${platform}: None
    > src/tools/resultdb:infra/tools/result_adapter/${platform}: None
    > ```
    >
    > Bug:b/279097790
    > Change-Id: I21abf2579910e9d79d9ee0dcd018ee761e3d3c1c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4463983
    > Reviewed-by: Rachael Newitt <renewitt@google.com>
    > Reviewed-by: Gavin Mak <gavinmak@google.com>
    > Commit-Queue: Dan Le Febvre <dlf@google.com>
    
    Bug: b/279097790
    Change-Id: Ib08dac506ce221243c595dde5cb00e8e0d7dc7ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4501582
    Reviewed-by: Gavin Mak <gavinmak@google.com>
    Commit-Queue: Dan Le Febvre <dlf@google.com>

--- testing_support/fake_cipd.py
+++ testing_support/fake_cipd.py
@@ -10,8 +10,14 @@ import re
 import shutil
 import sys
 
+ARCH_VAR = 'arch'
+OS_VAR = 'os'
+PLATFORM_VAR = 'platform'
 
 CIPD_SUBDIR_RE = '@Subdir (.*)'
+CIPD_ENSURE = 'ensure'
+CIPD_EXPAND_PKG = 'expand-package-name'
+CIPD_EXPORT = 'export'
 
 
 def parse_cipd(root, contents):
@@ -29,8 +35,32 @@ def parse_cipd(root, contents):
   return tree
 
 
+def expand_package_name_cmd(package_name):
+  package_split = package_name.split("/")
+  suffix = package_split[-1]
+  # Any use of var equality should return empty for testing.
+  if "=" in suffix:
+    if suffix != "${platform=fake-platform-ok}":
+      return ""
+    package_name = "/".join(package_split[:-1] + ["${platform}"])
+
+  for v in [ARCH_VAR, OS_VAR, PLATFORM_VAR]:
+    var = "${%s}" % v
+    if package_name.endswith(var):
+      package_name = package_name.replace(var, "%s-expanded-test-only" % v)
+  return package_name
+
+
 def main():
-  assert sys.argv[1] in ['ensure', 'export']
+  cmd = sys.argv[1]
+  assert cmd in [CIPD_ENSURE, CIPD_EXPAND_PKG, CIPD_EXPORT]
+  # Handle cipd expand-package-name
+  if cmd == CIPD_EXPAND_PKG:
+    # Expecting argument after cmd
+    assert len(sys.argv) == 3
+    # Write result to stdout
+    sys.stdout.write(expand_package_name_cmd(sys.argv[2]))
+    return 0
   parser = argparse.ArgumentParser()
   parser.add_argument('-ensure-file')
   parser.add_argument('-root')
