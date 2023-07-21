d411904b84c0d9f539bbdc84e3d63f9f282c658b
Ben Segall <bentekkie@google.com>
2023-07-21 17:17:13 +0000

    [reclient] disable racing for now until it is less agressive
    Bug: b/288564971
    Change-Id: I06d1421ff7de353d6e87be33ac3dca8e06ff6880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4704566
    Auto-Submit: Ben Segall <bentekkie@google.com>
    Reviewed-by: Andus Yu <andusyu@google.com>
    Commit-Queue: Ben Segall <bentekkie@google.com>

--- reclient_helper.py
+++ reclient_helper.py
@@ -168,11 +168,6 @@ def set_reproxy_path_flags(out_dir, make_dirs=True):
         hashlib.sha256(tmp_dir.encode()).hexdigest())
 
 
-def enable_racing():
-  os.environ.setdefault("RBE_exec_strategy", "racing")
-  # TODO(b/288285261) Tune bias once latency data has been gathered.
-  os.environ.setdefault("RBE_racing_bias", "0.95")
-
 
 @contextlib.contextmanager
 def build_context(argv, tool):
@@ -206,8 +201,6 @@ def build_context(argv, tool):
   if os.environ.get('RBE_instance', None):
     print('WARNING: Using RBE_instance=%s\n' %
           os.environ.get('RBE_instance', ''))
-
-  enable_racing()
 
   reproxy_ret_code = start_reproxy(reclient_cfg, reclient_bin_dir)
   if reproxy_ret_code != 0:
