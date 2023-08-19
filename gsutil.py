b11693a5cab86ffe6be648fc39818daac33027d8
Josip Sokcevic <sokcevic@chromium.org>
2023-08-19 00:30:41 +0000

    Reland "[gsutil] Update gsutil error message on outdated boto"
    This is a reland of commit dfafd0a3c798f7389c983adf39f53537de009a66
    
    Original change's description:
    > [gsutil] Update gsutil error message on outdated boto
    >
    > Add information where .boto file is located
    >
    > Change-Id: Id8605196ab0906a594bbb0c87a016986415eedac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4787527
    > Reviewed-by: Aravind Vasudevan <aravindvasudev@google.com>
    > Commit-Queue: Josip Sokcevic <sokcevic@chromium.org>
    
    Change-Id: Id48ee21837f326795bca25beedced6478f5cc791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4792027
    Reviewed-by: Aravind Vasudevan <aravindvasudev@google.com>
    Auto-Submit: Josip Sokcevic <sokcevic@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Aravind Vasudevan <aravindvasudev@google.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>

--- gsutil.py
+++ gsutil.py
@@ -262,12 +262,12 @@ def run_gsutil(target, args, clean=False):
     if b'Your credentials are invalid.' in p.stderr:
       # Make sure this error message is visible when invoked by gclient runhooks
       separator = '*' * 80
-      print(
-          '\n' + separator + '\n' +
-          'Warning: You might have an outdated .boto file. If this issue '
-          'persists after running `gsutil.py config`, try removing your '
-          '.boto file.\n' + separator + '\n',
-          file=sys.stderr)
+      print('\n' + separator + '\n' +
+            'Warning: You might have an outdated .boto file. If this issue '
+            'persists after running `gsutil.py config`, try removing your '
+            '.boto, usually located in your home directory.\n' + separator +
+            '\n',
+            file=sys.stderr)
 
     _print_subprocess_result(p)
     return p.returncode
