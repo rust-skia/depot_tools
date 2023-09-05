f38dc929a88633e54d1911ba94b2b37a6c164238
Mike Frysinger <vapier@chromium.org>
2023-09-05 20:14:34 +0000

    testing_support: switch to 4 space indent
    Reformat this dir by itself to help merging with conflicts with other CLs.
    
    Reformatted using:
    parallel ./yapf -i -- testing_support/*.py
    ~/chromiumos/chromite/contrib/reflow_overlong_comments testing_support/*.py
    
    The files that still had strings that were too long were manually
    reformatted.
    testing_support/coverage_utils.py
    testing_support/fake_repos.py
    testing_support/git_test_utils.py
    testing_support/presubmit_canned_checks_test_mocks.py
    
    Change-Id: I4726a4bbd279a70bcf65d0987fcff0ff9a231386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/4842593
    Reviewed-by: Josip Sokcevic <sokcevic@chromium.org>
    Commit-Queue: Josip Sokcevic <sokcevic@chromium.org>
    Auto-Submit: Mike Frysinger <vapier@chromium.org>

--- testing_support/coverage_utils.py
+++ testing_support/coverage_utils.py
@@ -10,22 +10,26 @@ import sys
 import textwrap
 import unittest
 
-ROOT_PATH = os.path.abspath(os.path.join(
-    os.path.dirname(os.path.dirname(__file__))))
+ROOT_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.dirname(__file__))))
 
 
 def native_error(msg, version):
-  print(textwrap.dedent("""\
+    print(
+        textwrap.dedent("""\
   ERROR: Native python-coverage (version: %s) is required to be
   installed on your PYTHONPATH to run this test. Recommendation:
      sudo apt-get install pip
      sudo pip install --upgrade coverage
   %s""") % (version, msg))
-  sys.exit(1)
+    sys.exit(1)
 
-def covered_main(includes, require_native=None, required_percentage=100.0,
+
+def covered_main(includes,
+                 require_native=None,
+                 required_percentage=100.0,
                  disable_coverage=True):
-  """Equivalent of unittest.main(), except that it gathers coverage data, and
+    """Equivalent of unittest.main(), except that it gathers coverage data, and
   asserts if the test is not at 100% coverage.
 
   Args:
@@ -37,43 +41,45 @@ def covered_main(includes, require_native=None, required_percentage=100.0,
     disable_coverage (bool) - If True, just run unittest.main() without any
       coverage tracking. Bug: crbug.com/662277
   """
-  if disable_coverage:
-    unittest.main()
-    return
+    if disable_coverage:
+        unittest.main()
+        return
 
-  try:
-    import coverage
-    if require_native is not None:
-      got_ver = coverage.__version__
-      if not getattr(coverage.collector, 'CTracer', None):
-        native_error((
-            "Native python-coverage module required.\n"
-            "Pure-python implementation (version: %s) found: %s"
-          ) % (got_ver, coverage), require_native)
-      if got_ver < distutils.version.LooseVersion(require_native):
-        native_error("Wrong version (%s) found: %s" % (got_ver, coverage),
-                     require_native)
-  except ImportError:
-    if require_native is None:
-      sys.path.insert(0, os.path.join(ROOT_PATH, 'third_party'))
-      import coverage
-    else:
-      print("ERROR: python-coverage (%s) is required to be installed on your "
-             "PYTHONPATH to run this test." % require_native)
-      sys.exit(1)
+    try:
+        import coverage
+        if require_native is not None:
+            got_ver = coverage.__version__
+            if not getattr(coverage.collector, 'CTracer', None):
+                native_error(
+                    ("Native python-coverage module required.\n"
+                     "Pure-python implementation (version: %s) found: %s") %
+                    (got_ver, coverage), require_native)
+            if got_ver < distutils.version.LooseVersion(require_native):
+                native_error(
+                    "Wrong version (%s) found: %s" % (got_ver, coverage),
+                    require_native)
+    except ImportError:
+        if require_native is None:
+            sys.path.insert(0, os.path.join(ROOT_PATH, 'third_party'))
+            import coverage
+        else:
+            print(
+                "ERROR: python-coverage (%s) is required to be installed on "
+                "your PYTHONPATH to run this test." % require_native)
+            sys.exit(1)
 
-  COVERAGE = coverage.coverage(include=includes)
-  COVERAGE.start()
+    COVERAGE = coverage.coverage(include=includes)
+    COVERAGE.start()
 
-  retcode = 0
-  try:
-    unittest.main()
-  except SystemExit as e:
-    retcode = e.code or retcode
+    retcode = 0
+    try:
+        unittest.main()
+    except SystemExit as e:
+        retcode = e.code or retcode
 
-  COVERAGE.stop()
-  if COVERAGE.report() < required_percentage:
-    print('FATAL: not at required %f%% coverage.' % required_percentage)
-    retcode = 2
+    COVERAGE.stop()
+    if COVERAGE.report() < required_percentage:
+        print('FATAL: not at required %f%% coverage.' % required_percentage)
+        retcode = 2
 
-  return retcode
+    return retcode
--- testing_support/fake_cipd.py
+++ testing_support/fake_cipd.py
@@ -74,129 +74,131 @@ DESCRIBE_JSON_TEMPLATE = """{
 
 
 def parse_cipd(root, contents):
-  tree = {}
-  current_subdir = None
-  for line in contents:
-    line = line.strip()
-    match = re.match(CIPD_SUBDIR_RE, line)
-    if match:
-      print('match')
-      current_subdir = os.path.join(root, *match.group(1).split('/'))
-      if not root:
-        current_subdir = match.group(1)
-    elif line and current_subdir:
-      print('no match')
-      tree.setdefault(current_subdir, []).append(line)
-  return tree
+    tree = {}
+    current_subdir = None
+    for line in contents:
+        line = line.strip()
+        match = re.match(CIPD_SUBDIR_RE, line)
+        if match:
+            print('match')
+            current_subdir = os.path.join(root, *match.group(1).split('/'))
+            if not root:
+                current_subdir = match.group(1)
+        elif line and current_subdir:
+            print('no match')
+            tree.setdefault(current_subdir, []).append(line)
+    return tree
 
 
 def expand_package_name_cmd(package_name):
-  package_split = package_name.split("/")
-  suffix = package_split[-1]
-  # Any use of var equality should return empty for testing.
-  if "=" in suffix:
-    if suffix != "${platform=fake-platform-ok}":
-      return ""
-    package_name = "/".join(package_split[:-1] + ["${platform}"])
-  for v in [ARCH_VAR, OS_VAR, PLATFORM_VAR]:
-    var = "${%s}" % v
-    if package_name.endswith(var):
-      package_name = package_name.replace(var, "%s-expanded-test-only" % v)
-  return package_name
+    package_split = package_name.split("/")
+    suffix = package_split[-1]
+    # Any use of var equality should return empty for testing.
+    if "=" in suffix:
+        if suffix != "${platform=fake-platform-ok}":
+            return ""
+        package_name = "/".join(package_split[:-1] + ["${platform}"])
+    for v in [ARCH_VAR, OS_VAR, PLATFORM_VAR]:
+        var = "${%s}" % v
+        if package_name.endswith(var):
+            package_name = package_name.replace(var,
+                                                "%s-expanded-test-only" % v)
+    return package_name
 
 
 def ensure_file_resolve():
-  resolved = {"result": {}}
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-ensure-file', required=True)
-  parser.add_argument('-json-output')
-  args, _ = parser.parse_known_args()
-  with io.open(args.ensure_file, 'r', encoding='utf-8') as f:
-    new_content = parse_cipd("", f.readlines())
-    for path, packages in new_content.items():
-      resolved_packages = []
-      for package in packages:
-        package_name = expand_package_name_cmd(package.split(" ")[0])
-        resolved_packages.append({
-            "package": package_name,
-            "pin": {
-                "package": package_name,
-                "instance_id": package_name + "-fake-resolved-id",
-            }
-        })
-      resolved["result"][path] = resolved_packages
-    with io.open(args.json_output, 'w', encoding='utf-8') as f:
-      f.write(json.dumps(resolved, indent=4))
+    resolved = {"result": {}}
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ensure-file', required=True)
+    parser.add_argument('-json-output')
+    args, _ = parser.parse_known_args()
+    with io.open(args.ensure_file, 'r', encoding='utf-8') as f:
+        new_content = parse_cipd("", f.readlines())
+        for path, packages in new_content.items():
+            resolved_packages = []
+            for package in packages:
+                package_name = expand_package_name_cmd(package.split(" ")[0])
+                resolved_packages.append({
+                    "package": package_name,
+                    "pin": {
+                        "package": package_name,
+                        "instance_id": package_name + "-fake-resolved-id",
+                    }
+                })
+            resolved["result"][path] = resolved_packages
+        with io.open(args.json_output, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(resolved, indent=4))
 
 
 def describe_cmd(package_name):
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-json-output')
-  parser.add_argument('-version', required=True)
-  args, _ = parser.parse_known_args()
-  json_template = Template(DESCRIBE_JSON_TEMPLATE).substitute(
-      package=package_name)
-  cli_out = Template(DESCRIBE_STDOUT_TEMPLATE).substitute(package=package_name)
-  json_out = json.loads(json_template)
-  found = False
-  for tag in json_out['result']['tags']:
-    if tag['tag'] == args.version:
-      found = True
-      break
-  for tag in json_out['result']['refs']:
-    if tag['ref'] == args.version:
-      found = True
-      break
-  if found:
-    if args.json_output:
-      with io.open(args.json_output, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(json_out, indent=4))
-    sys.stdout.write(cli_out)
-    return 0
-  sys.stdout.write('Error: no such ref.\n')
-  return 1
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-json-output')
+    parser.add_argument('-version', required=True)
+    args, _ = parser.parse_known_args()
+    json_template = Template(DESCRIBE_JSON_TEMPLATE).substitute(
+        package=package_name)
+    cli_out = Template(DESCRIBE_STDOUT_TEMPLATE).substitute(
+        package=package_name)
+    json_out = json.loads(json_template)
+    found = False
+    for tag in json_out['result']['tags']:
+        if tag['tag'] == args.version:
+            found = True
+            break
+    for tag in json_out['result']['refs']:
+        if tag['ref'] == args.version:
+            found = True
+            break
+    if found:
+        if args.json_output:
+            with io.open(args.json_output, 'w', encoding='utf-8') as f:
+                f.write(json.dumps(json_out, indent=4))
+        sys.stdout.write(cli_out)
+        return 0
+    sys.stdout.write('Error: no such ref.\n')
+    return 1
 
 
 def main():
-  cmd = sys.argv[1]
-  assert cmd in [
-      CIPD_DESCRIBE, CIPD_ENSURE, CIPD_ENSURE_FILE_RESOLVE, CIPD_EXPAND_PKG,
-      CIPD_EXPORT
-  ]
-  # Handle cipd expand-package-name
-  if cmd == CIPD_EXPAND_PKG:
-    # Expecting argument after cmd
-    assert len(sys.argv) == 3
-    # Write result to stdout
-    sys.stdout.write(expand_package_name_cmd(sys.argv[2]))
+    cmd = sys.argv[1]
+    assert cmd in [
+        CIPD_DESCRIBE, CIPD_ENSURE, CIPD_ENSURE_FILE_RESOLVE, CIPD_EXPAND_PKG,
+        CIPD_EXPORT
+    ]
+    # Handle cipd expand-package-name
+    if cmd == CIPD_EXPAND_PKG:
+        # Expecting argument after cmd
+        assert len(sys.argv) == 3
+        # Write result to stdout
+        sys.stdout.write(expand_package_name_cmd(sys.argv[2]))
+        return 0
+    if cmd == CIPD_DESCRIBE:
+        # Expecting argument after cmd
+        assert len(sys.argv) >= 3
+        return describe_cmd(sys.argv[2])
+    if cmd == CIPD_ENSURE_FILE_RESOLVE:
+        return ensure_file_resolve()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ensure-file')
+    parser.add_argument('-root')
+    args, _ = parser.parse_known_args()
+
+    with io.open(args.ensure_file, 'r', encoding='utf-8') as f:
+        new_content = parse_cipd(args.root, f.readlines())
+
+    # Install new packages
+    for path, packages in new_content.items():
+        if not os.path.exists(path):
+            os.makedirs(path)
+        with io.open(os.path.join(path, '_cipd'), 'w', encoding='utf-8') as f:
+            f.write('\n'.join(packages))
+
+    # Save the ensure file that we got
+    shutil.copy(args.ensure_file, os.path.join(args.root, '_cipd'))
+
     return 0
-  if cmd == CIPD_DESCRIBE:
-    # Expecting argument after cmd
-    assert len(sys.argv) >= 3
-    return describe_cmd(sys.argv[2])
-  if cmd == CIPD_ENSURE_FILE_RESOLVE:
-    return ensure_file_resolve()
-
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-ensure-file')
-  parser.add_argument('-root')
-  args, _ = parser.parse_known_args()
-
-  with io.open(args.ensure_file, 'r', encoding='utf-8') as f:
-    new_content = parse_cipd(args.root, f.readlines())
-
-  # Install new packages
-  for path, packages in new_content.items():
-    if not os.path.exists(path):
-      os.makedirs(path)
-    with io.open(os.path.join(path, '_cipd'), 'w', encoding='utf-8') as f:
-      f.write('\n'.join(packages))
-
-  # Save the ensure file that we got
-  shutil.copy(args.ensure_file, os.path.join(args.root, '_cipd'))
-
-  return 0
 
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
