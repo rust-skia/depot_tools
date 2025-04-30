e6596746dc95fe658b3a5f0924c10e322c7d8e22
Fumitoshi Ukai <ukai@google.com>
2025-04-30 10:26:53 -0700

    better error message from python3 when not initialized
    Bug: 414330998
    Change-Id: I47fcd9571ba3de0a080e31624f9bdf0fa738914a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/6498079
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Gavin Mak <gavinmak@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Gavin Mak <gavinmak@google.com>

--- python-bin/python3.bat
+++ python-bin/python3.bat
@@ -6,6 +6,13 @@
 setlocal
 
 for %%d in (%~dp0..) do set PARENT_DIR=%%~fd
+IF NOT EXIST "%PARENT_DIR%\python3_bin_reldir.txt" (
+  echo python3_bin_reldir.txt not found. need to initialize depot_tools by >&2
+  echo running gclient or update_depot_tools >&2
+  exit /b 1
+)
+
+:Initialized
 for /f %%i in (%PARENT_DIR%\python3_bin_reldir.txt) do set PYTHON_BIN_ABSDIR=%PARENT_DIR%\%%i
 set PATH=%PYTHON_BIN_ABSDIR%;%PYTHON_BIN_ABSDIR%\Scripts;%PATH%
 "%PYTHON_BIN_ABSDIR%\python3.exe" %*
