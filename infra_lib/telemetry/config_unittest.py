46ad34abcadcf57ec43258d90573f013887be16e
Struan Shrimpton <sshrimp@google.com>
2025-06-05 12:59:57 -0700

    Reland "telemetry: Collect until opt-out"
    This is a reland of commit 9c11bcef1f80da68b53ab4de64a4b75e884977b4
    
    Original change's description:
    > telemetry: Collect until opt-out
    >
    > The pdd says we will collect by default:
    > https://eldar.corp.google.com/assessments/570486509/revisions/1/sections/550004#questions/550404/revisions/2
    >
    > Also reduce the notice count to 9 since it notices on "0 remaining"
    >
    > Bug: 326277821
    > Change-Id: I4de584c36868b45cefaa5cea42f88d245485dce7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/6616983
    > Reviewed-by: Ben Pastene <bpastene@chromium.org>
    > Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    
    Bug: 326277821
    Change-Id: Ieee21e20ede1dfa71e6c7944ba0e92e74b5a5ded
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/6624994
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>

--- infra_lib/telemetry/config_unittest.py
+++ infra_lib/telemetry/config_unittest.py
@@ -21,12 +21,12 @@ class ConfigTest(unittest.TestCase):
             cfg = config.Config(path)
 
             with open(path, 'r') as f:
-                self.assertEqual(
-                    f.read(), "[root]\nnotice_countdown = 10\n\n[trace]\n\n")
+                self.assertEqual(f.read(),
+                                 "[root]\nnotice_countdown = 9\n\n[trace]\n\n")
             self.assertFalse(cfg.trace_config.enabled)
             self.assertFalse(cfg.trace_config.has_enabled())
             self.assertEqual("AUTO", cfg.trace_config.enabled_reason)
-            self.assertEqual(10, cfg.root_config.notice_countdown)
+            self.assertEqual(9, cfg.root_config.notice_countdown)
 
     def test_load_config_file(self) -> None:
         """Test Config to load config file."""
@@ -60,19 +60,17 @@ class ConfigTest(unittest.TestCase):
             cfg.flush()
 
             with open(path, 'r') as f:
-                self.assertEqual(
-                    f.read(),
+                file = f.read()
+                self.assertIn(
                     "\n".join([
                         "[root]",
                         "notice_countdown = 9",
-                        "",
-                        "[trace]",
-                        "enabled = False",
-                        "enabled_reason = AUTO",
-                        "",
-                        "",
                     ]),
+                    file,
                 )
+                self.assertIn("[trace]", file)
+                self.assertIn("enabled = False", file)
+                self.assertIn("enabled_reason = AUTO", file)
 
 
 def test_default_trace_config() -> None:
@@ -111,7 +109,7 @@ def test_default_root_config() -> None:
     cfg[config.ROOT_SECTION_KEY] = {}
     root_config = config.RootConfig(cfg)
 
-    assert root_config.notice_countdown == 10
+    assert root_config.notice_countdown == 9
 
 
 def test_root_config_update() -> None:
